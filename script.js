628df350f38027769180ffffaf9f517781ed4f01
leitane-_i <indra.leitane@gmail.com>
2025-12-29 01:37:36 +0200

    final?

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const generatePattern = () => {
     }
 
     if (end > 100000) {
-        alert("Limit: Maximum value for 'End' is 100,000 to protect browser performance.");
+        alert("Limit: Maximum value is 100000.");
         end = 100000;
         document.getElementById('endVal').value = 100000;
     }
@@ -33,23 +33,36 @@ const generatePattern = () => {
     const sideSize = Math.ceil(Math.sqrt(totalElements));
     container.style.gridTemplateColumns = `repeat(${sideSize}, 1fr)`;
 
+    let fontSize = "0.8rem";
+    if (sideSize > 15) fontSize = "0.5rem";
+    if (sideSize > 30) fontSize = "0.2rem";
+
     const fragment = document.createDocumentFragment();
 
     for (let i = start; i <= end; i++) {
         const pixel = document.createElement('div');
         pixel.classList.add('box');
+        pixel.id = "myid" + i;
+        pixel.style.fontSize = fontSize;
 
         const isFizz = i % fizzDiv === 0;
         const isBuzz = i % buzzDiv === 0;
 
         if (isFizz && isBuzz) {
             pixel.classList.add('fizzbuzz');
+            if (end <= 100) pixel.innerText = "FizzBuzz";
+            else if (end <= 400) pixel.innerText = i;
         } else if (isFizz) {
             pixel.classList.add('fizz');
+            if (end <= 100) pixel.innerText = "Fizz";
+            else if (end <= 400) pixel.innerText = i;
         } else if (isBuzz) {
             pixel.classList.add('buzz');
+            if (end <= 100) pixel.innerText = "Buzz";
+            else if (end <= 400) pixel.innerText = i;
         } else {
             pixel.classList.add('none');
+            if (end <= 400) pixel.innerText = i;
         }
 
         fragment.appendChild(pixel);
@@ -57,7 +70,6 @@ const generatePattern = () => {
 
     container.appendChild(fragment);
 };
-
 
 btnGenerate.addEventListener('click', generatePattern);
 btnClear.addEventListener('click', clearContainer);
